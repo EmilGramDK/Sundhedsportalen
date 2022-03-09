7e720901b210c8c56de6d96929ecd7477111a504
Emil Gram <emilgram2001@gmail.com>
2022-03-09 13:55:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Health_Care_System
+ 
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+const viewsDirPath = path.join(__dirname, "templates", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", viewsDirPath);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "p2login" && password === "12345") {
+    res.render("success", {
+      username: username,
+    });
+  } else {
+    res.render("failure");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
