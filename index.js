5e66ec81ec93e914b9e26522f1a2cb5cc803b3f6
Emil Gram <emilgram2001@gmail.com>
2022-03-09 21:42:43 +0100

    Added mysql database
    Updated website theme and added mysql database

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
+require("dotenv").config();
 const express = require("express");
+const session = require('express-session');
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+app.use(session({
+	secret: 'secrethash',
+	resave: true,
+	saveUninitialized: true
+}));
+const mysql = require("mysql");
+const { PORT, DB_HOST, DB_NAME, DB_PORT, DB_USER_NAME, DB_USER_PASSWORD } =
+  process.env;
 
+// setup view path
 const viewsDirPath = path.join(__dirname, "templates", "views");
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", viewsDirPath);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+// connect to the database
+const dbConn = mysql.createConnection({
+  host: DB_HOST || "",
+  user: DB_USER_NAME || "",
+  password: DB_USER_PASSWORD || "",
+  database: DB_NAME || "",
 });
 
-app.get("/login", (req, res) => {
-  res.render("login", {
-    info: "Brugernavn: admin - Adgangskode: 123"
-  });
-});
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (username === "admin" && password === "123") {
-    res.render("success", {
-      username: username,
-    });
-  } else {
-    res.render("login", {
-      error: "Forkert brugernavn eller adgangskode"
-    });
+dbConn.connect(function (err) {
+  if (err) {
+    console.log(err);
+    throw err;
   }
-});
 
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+  console.log("Connected to the database!");
+
+  // include website routes
+  require('./routes')({ app, dbConn });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 });
--- public/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("This is coming from script.js");
