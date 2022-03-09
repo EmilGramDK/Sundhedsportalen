c2f046425646729e44ec1d734330b234e8b538e1
Emil Gram <emilgram2001@gmail.com>
2022-03-09 17:00:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
