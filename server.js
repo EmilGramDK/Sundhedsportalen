21fbad592d55b7909029b6527db680e5959c3a3e
Emil Gram <emilgram2001@gmail.com>
2022-03-09 22:17:16 +0100

    added cookie support

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const session = require('express-session');
+const session = require('cookie-session');
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const path = require("path");
