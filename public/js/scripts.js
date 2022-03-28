c1aa02cac742861406b3359fe1c0df35d7468b6e
MGLarsenSW <mathias.larsen9@gmail.com>
2022-03-28 12:44:28 +0200

    added datalist

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,12 @@
 console.log("This is coming from script.js");
+
+function onChangeDatalistInput() {
+    let input = document.getElementsByClassName("datainput")[0].value;
+    let datalist = document.getElementsByClassName("datalist")[0];
+
+    if (input.length >= 2) {
+        datalist.id = "myList";
+    } else {
+        datalist.removeAttribute("id");
+    }
+}
